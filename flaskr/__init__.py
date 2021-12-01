dc3a28199757ccb840f87fcb6fad0eeae0ad1d86
renantescaro <renantescaro@yahoo.com.br>
2021-12-01 13:38:30 -0300

    arquivo inicial

---
+++ flaskr/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flaskr.controllers.inicial_ctrl import bp as bp_inicial
+from flaskr.controllers.produto_ctrl import bp as bp_produto
+from flaskr.controllers.populacao_ctrl import bp as bp_populacao
+from flaskr.controllers.municipio_ctrl import bp as bp_municipio
+from flaskr.controllers.pesquisa_ctrl import bp as bp_pesquisa
+
+
+def create_app(test_config=None):
+    app = Flask(
+        __name__,
+        static_url_path = '/static',
+        static_folder   = 'static',
+        instance_relative_config = True )
+
+    app.config.from_mapping(
+        SECRET_KEY   = 'super secret key',
+        SESSION_TYPE = 'filesystem',
+        JSONIFY_PRETTYPRINT_REGULAR = False )
+
+
+    # adicionar rotas
+    app.register_blueprint(bp_inicial)
+    app.register_blueprint(bp_produto)
+    app.register_blueprint(bp_populacao)
+    app.register_blueprint(bp_municipio)
+    app.register_blueprint(bp_pesquisa)
+    return app
